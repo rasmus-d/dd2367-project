2b1597a085acf3f292590a887e43b41243479462
Rasmus Danielsson <rasmus@danielsson.dev>
2024-10-18 15:16:21 +0200

    add test skeleton

---
+++ test/test_sim.py
@@ -0,0 +1,15 @@
+'''
+If we want to add some tests later.
+Probably a good idea but might just eat valuable time
+'''
+import unittest
+
+import src
+
+
+class TestQuantumSimulator(unittest.TestCase):
+
+    def test_hadamard(self):
+        pass
+    # etc..
+
