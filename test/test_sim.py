6691003c9a4338fd8b9d2a77e2dc15199cf08d92
Christopher Sapinski <sapinski@kth.se>
2025-01-02 18:56:31 -0800

    more tests

--- test/test_sim.py
+++ test/test_sim.py
@@ -9,6 +9,7 @@ from src import *
 def showplot(qc):
     qc.circplot()
     plt.savefig('test.png')
+    plt.close()
 
 class TestQuantumSimulator(unittest.TestCase):
 
@@ -21,6 +22,13 @@ class TestQuantumSimulator(unittest.TestCase):
         chance1 = qc.state.dict[0]
         self.assertAlmostEqual(chance0 * chance0, 0.5)
         self.assertAlmostEqual(chance1 * chance1, 0.5)
+
+        qc = QuantumSimulator(1)
+        qc.add(H(0))
+        qc.add(H(0))
+        qc.run()
+        self.assertAlmostEqual(qc.state.dict[0], 1)
+
     def test_measure_hadamard(self):
         test = 10000
         count = 0 
@@ -68,10 +76,97 @@ class TestQuantumSimulator(unittest.TestCase):
         self.assertEqual(qc.state.dict[1], -1)
     
     def test_S(self):
-        qc = QuantumSimulator(1, State({0: 1}))
+        qc = QuantumSimulator(1, State({1: 1}))
         qc.add(S(0))
+
         qc.run()
-        print(qc.state.dict)
+        self.assertEqual(qc.state.dict[1], 1j)
+        qc.run()
+        self.assertEqual(qc.state.dict[1], -1)
+        qc.run()
+        self.assertEqual(qc.state.dict[1], -1j)
+        qc.run()
+        self.assertEqual(qc.state.dict[1], 1)
+    
+    def test_T(self):
+        qc = QuantumSimulator(1, State({1: 1}))
+        qc.add(T(0))
+        qc.add(T(0))
+
+        qc.run()
+        self.assertAlmostEqual(qc.state.dict[1], 1j)
+        qc.run()
+        self.assertAlmostEqual(qc.state.dict[1], -1)
+        qc.run()
+        self.assertAlmostEqual(qc.state.dict[1], -1j)
+        qc.run()
+        self.assertAlmostEqual(qc.state.dict[1], 1)
+    
+    def test_RX(self):
+        qc = QuantumSimulator(1, State({1: 1}))
+        qc.add(RX(0, pi))
+        qc.run()
+        self.assertEqual(qc.state.dict[0], -1j)
+
+        qc = QuantumSimulator(1, State({1: 1}))
+        qc.add(RX(0, 2*pi))
+        qc.run()
+        self.assertEqual(qc.state.dict[1], -1)
+
+    def test_RY(self):
+        qc = QuantumSimulator(1, State({1: 1}))
+        qc.add(RY(0, pi))
+        qc.run()
+        self.assertEqual(qc.state.dict[0], -1)
+
+        qc = QuantumSimulator(1, State({1: 1}))
+        qc.add(RY(0, 2*pi))
+        qc.run()
+        self.assertEqual(qc.state.dict[1], -1)
+    
+    def test_RZ(self):
+        qc = QuantumSimulator(1, State({1: 1}))
+        qc.add(RZ(0, pi))
+        qc.run()
+        self.assertAlmostEqual(qc.state.dict[1], 1j)
+
+        qc = QuantumSimulator(1, State({1: 1}))
+        qc.add(RZ(0, 2*pi))
+        qc.run()
+        self.assertAlmostEqual(qc.state.dict[1], -1)
+
+    def test_P(self):
+        qc = QuantumSimulator(1, State({1: 1}))
+        qc.add(P(0, pi))
+        qc.run()
+        self.assertAlmostEqual(qc.state.dict[1], -1)
+
+        qc = QuantumSimulator(1, State({1: 1}))
+        qc.add(P(0, pi / 2))
+        qc.run()
+        self.assertAlmostEqual(qc.state.dict[1], 1j)
+
+    def test_SWAP(self):
+        qc = QuantumSimulator(2, State({1: 1}))
+        qc.add(SWAP(0, 1))
+        qc.run()
+        self.assertEqual(qc.state.dict[2], 1)
+
+        qc = QuantumSimulator(2, State({2: 1}))
+        qc.add(SWAP(0, 1))
+        qc.run()
+        self.assertEqual(qc.state.dict[1], 1)
+
+        qc = QuantumSimulator(2, State({3: 1}))
+        self.assertEqual(qc.state.dict[3], 1)
+        qc.add(SWAP(0, 1))
+        qc.run()
+        self.assertEqual(qc.state.dict[3], 1)
+    
+    def test_control(self):
+        qc = QuantumSimulator(3, State({1: 1}))
+        qc.add(SWAP(0, 1, [3]))
+        qc.run()
         showplot(qc)
 
 if __name__ == '__main__':
